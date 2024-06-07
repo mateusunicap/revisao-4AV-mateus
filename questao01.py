307d6a97eeef0fcac96ea4f556dea9bafbf72b71
mateusunicap <mateus.00000844534@unicap.br>
2024-06-07 08:55:13 -0300

    primeira questão

---
+++ questao01.py
@@ -0,0 +1,12 @@
+# 1)Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+
+
+def maior(* numeros):
+    maior_elemento = numeros[0]
+    for i in range(1, len(numeros)):
+        maior_elemento = numeros[i]
+    print(maior_elemento)
+
+
+maior(1, 2, 3, 4)
+maior(100, 2)
